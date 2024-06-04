64407fc6b0ab7479970efc2a8a012145ee5b3e68
Salamonstrer <117446754+Salamonstrer@users.noreply.github.com>
2024-06-04 15:52:58 +0200

    Update keepers.sol

--- keepers.sol
+++ keepers.sol
@@ -7,15 +7,15 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract Cryptopunkstar is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+contract Salamonstrer is ERC20, ERC20Burnable, Ownable, ERC20Permit {
     constructor(address initialOwner)
-        ERC20("Cryptopunkstar", "CPS")
+        ERC20("Salamonstrer", "CPS")
         Ownable(initialOwner)
-        ERC20Permit("Cryptopunkstar")
+        ERC20Permit("Salamonstrer")
     {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
-//for mintable cryptopunkstar
+//for mintable Salamonstrer
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
